49e3d76e6ac7226e8dd9852e3930a1d8a3186081
KALYANI <22b01a12a0@svecw.edu.in>
2024-07-25 10:08:56 +0530

    COMMITzo0us77ku

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App(){
            <Routes>
             <Route path="/" element={<DataFetching/>} ></Route>
             <Route path="/products" element={<Assign3/>} ></Route>
+            <Route path="/cards" element={<Cards/>} ></Route>
             
            </Routes>
       </BrowserRouter>
